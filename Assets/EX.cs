ae93e7afbb6e3a48f36592d5d7dde5278c624e9b
DESKTOP-RPILFTG\drivi <driving.ability@gmail.com>
2024-07-23 19:50:33 +0900

    tttt

--- Assets/EX.cs
+++ Assets/EX.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class EX : MonoBehaviour
 {
+
+    public GameObject slope;
+
+    float gap;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Vector3 position = transform.position; // ローカル変数に格納
+
+        Vector3 s_position = slope.transform.position;
+
+        gap = s_position.x - position.x;
     }
 
     // Update is called once per frame
@@ -16,5 +26,17 @@ public class EX : MonoBehaviour
        /* if (Input.GetKey (KeyCode.LeftArrow)) {
        Debug.Log("MOVE");
     }*/
+
+    Vector3 position = transform.position; // ローカル変数に格納
+
+    Vector3 s_position = slope.transform.position;
+
+    s_position.x = position.x + gap;
+    
+    slope.transform.position = s_position; // ローカル変数を代入
+
+
+
+
     }
 }
